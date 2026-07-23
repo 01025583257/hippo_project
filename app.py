42192999672b49e4f513903646fd8abeade203d7
01025583257 <rahmardawod@gmail.com>
2026-07-23 23:41:00 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from tpot import TPOTClassifier
+from sklearn.ensemble import RandomForestClassifier
 
 # 1. Page Configuration
 st.set_page_config(layout="wide", page_title="NeuroImaging Platform", page_icon="🧠")
